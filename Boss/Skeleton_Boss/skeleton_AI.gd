0ddf1c464f62391291834f5bc927970fa9018bf1
HiggloH <hilding.hammarback@gmail.com>
2024-04-29 10:28:46 +0200

    skeleton idle state

--- Boss/Skeleton_Boss/skeleton_AI.gd
+++ Boss/Skeleton_Boss/skeleton_AI.gd
@@ -1,5 +1,66 @@
 extends CharacterBody2D
 
 enum States {
-	idle
+	Idle, #Fram och tillbaka ganska lågt
+	Return, #Åk upp till idle höjden från slutet av förra state
+	GroundSlam,
 }
+
+var current_state = States.Idle
+var direction = Vector2.ZERO
+
+@onready var anim_player = $AnimationPlayer
+
+#Idle variables
+@onready var max_right = $Idle/RightMax.global_position
+@onready var max_left = $Idle/LeftMax.global_position
+@onready var turn_timer = $Idle/TurnTimer
+var can_turn = true
+
+@export var idle_move_speed = 9500
+
+
+func change_state(new_state: States):
+	current_state = new_state
+	
+	if new_state == States.Idle:
+		direction = Vector2(1, 0)
+
+func _ready():
+	change_state(States.Idle)
+
+func _physics_process(delta):
+	match current_state:
+		States.Idle:
+			Idle(delta)
+		States.Return:
+			pass
+		States.GroundSlam:
+			pass
+
+func choose_state():
+	randomize()
+	var max = States.values().max()
+	var min = States.values().min() + 2 #plus två för att ta bort idle och return
+	
+	var new_attack = randi_range(min, max)
+
+func Return():
+	pass
+
+func Idle(delta):
+	if can_turn:
+		if global_position.distance_to(max_left) <= 1 and global_position.distance_to(max_left) >= -1:
+			direction.x = 1
+			can_turn = false
+			turn_timer.start()
+		if global_position.distance_to(max_right) <= 1 and global_position.distance_to(max_right) >= -1:
+			direction.x = -1
+			can_turn = false
+			turn_timer.start()
+	
+	velocity.x = direction.x * idle_move_speed * delta
+	move_and_slide()
+
+func _on_turn_timer_timeout():
+	can_turn = true
