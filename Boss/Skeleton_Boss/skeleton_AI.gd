222ce18cc58dbbcf403d1d56389504166606f28a
HiggloH <hilding.hammarback@gmail.com>
2024-04-30 10:37:11 +0200

    groundsmal

--- Boss/Skeleton_Boss/skeleton_AI.gd
+++ Boss/Skeleton_Boss/skeleton_AI.gd
@@ -8,6 +8,11 @@ enum States {
 
 var current_state = States.Idle
 var direction = Vector2.ZERO
+var gravity = 9.81
+
+var damage = -1
+
+@export var move_speed = 9500
 
 @onready var anim_player = $AnimationPlayer
 
@@ -17,47 +22,49 @@ var direction = Vector2.ZERO
 @onready var turn_timer = $Idle/TurnTimer
 var can_turn = true
 
-@export var idle_move_speed = 9500
-
 #Return Variables
 @onready var return_point = $Return/ReturnPoint
 
-var damage = -1
+#Ground Slam varaiables
+@onready var ground_collision = $GroundSlam/Area2D/CollisionShape2D
+@onready var wait_timer = $GroundSlam/WaitTimer
 
 func change_state(new_state: States):
 	direction = Vector2.ZERO
 	velocity = direction
 	
-	return_point.global_position = global_position
+	ground_collision.disabled = true
+	
+	if current_state == States.Idle:
+		return_point.global_position = global_position
 	
 	current_state = new_state
 	
 	if new_state == States.Idle:
 		direction = Vector2(1, 0)
 	if new_state == States.Return:
-		pass
-
-func _ready():
-	change_state(States.Idle)
+		direction = Vector2(0, -1)
 
 func _physics_process(delta):
 	match current_state:
 		States.Idle:
 			Idle(delta)
 		States.Return:
-			Return()
+			Return(delta)
 		States.GroundSlam:
-			pass
+			ground_slam(delta)
 
-func choose_state():
+func choose_attack_state():
 	randomize()
 	var max = States.values().max()
-	var min = States.values().min() + 2 #plus två för att ta bort idle och return
+	var min = States.values().min() + 2 #plus två för att ta bort idle, return och wait
 	
 	var new_attack = randi_range(min, max)
 
-func Return():
-	pass
+func Return(delta):
+	velocity = direction * move_speed * delta
+	
+	move_and_slide()
 
 func Idle(delta):
 	if can_turn:
@@ -70,14 +77,22 @@ func Idle(delta):
 			can_turn = false
 			turn_timer.start()
 	
-	velocity.x = direction.x * idle_move_speed * delta
+	velocity.x = direction.x * move_speed * delta
 	move_and_slide()
 
 func ground_slam(delta):
-	pass
+	if is_on_floor():
+		#ändra state och slå på collision
+		ground_collision.disabled = false
+	
+	velocity.y += gravity * delta
+	move_and_slide()
 
 func _on_turn_timer_timeout():
 	can_turn = true
 
 func get_damage():
 	return damage
+
+func _on_wait_timer_timeout():
+	change_state(States.Return)
