ab2948bd40f9f5a1838b56539f3c9d50ac2b6e9d
racke10 <rasmus.lindgren@minervagymnasium.se>
2024-05-20 10:57:24 +0200

    här får du P

--- Pause.gd
+++ Pause.gd
@@ -1,18 +1,29 @@
 extends Node2D
-
+#Rasmus skriver den här koden
+var In_Menu = 0
 
 func _ready():
-	process_mode = Node.PROCESS_MODE_ALWAYS
+	process_mode = Node.PROCESS_MODE_ALWAYS # kan inte pausa
 
 
 func _unhandled_input(event):
 	if event.is_action_pressed("Pause"):
-		if get_tree().paused == false:
-			get_tree().paused = true
-			$"Pause Menu".visible = true
-		elif get_tree().paused == true:
-			get_tree().paused = false
-			$"Pause Menu".visible = false
+		match In_Menu: # en avanserad and typ   ifall i menu åker du ut ifall i nåt annat back ett steg
+			0:
+				if get_tree().paused == false:
+					get_tree().paused = true
+					$"Pause Menu".visible = true
+				elif get_tree().paused == true:
+					get_tree().paused = false
+					$"Pause Menu".visible = false
+			1:
+				$"Pause Menu".visible = true
+				$Options_Menu.visible = false
+				In_Menu = 0
+			_:
+				print("Error menu out of bounds")
+		
+
 
 
 func _on_Main_menu_button_down():
@@ -24,7 +35,8 @@ func _on_Main_menu_button_down():
 func _on_Quit_button_down():
 	get_tree().quit()    # lämna spell
 
-
+#options funka
 func _on_Options_button_down():
-	visible = false
-	
+	In_Menu = 1
+	$"Pause Menu".visible = false
+	$Options_Menu.visible = true
