2ba68cda334ef1b1580446dfeb50f870849c7493
HiggloH <hilding.hammarback@gmail.com>
2024-05-27 10:52:51 +0200

    world start and sign done

--- Worlds/Levels/Objects/Sign/sign.gd
+++ Worlds/Levels/Objects/Sign/sign.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@onready var text_parent = $Text
+@onready var text_lable = $Text/RichTextLabel
+@export var text: String
+
+func _ready():
+	text_lable.text = text
+
 func _on_area_2d_area_entered(area):
 	#Show sign
-	pass
+	text_parent.visible = true
 
 func _on_area_2d_area_exited(area):
 	#Hide sign
-	pass # Replace with function body.
+	text_parent.visible = false
